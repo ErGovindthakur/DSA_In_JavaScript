59925d31ee500b2b1aa3f11f0798d83793bfb928
ErGovindthakur <ergovindthakur@gmail.com>
2025-04-14 11:03:28 +0530

    Optimizing code and including time and space complexity

--- 3_Again_Adv_Array/04_Array_RightRotationByKElements.js
+++ 3_Again_Adv_Array/04_Array_RightRotationByKElements.js
@@ -24,7 +24,7 @@ let rightRotationByKElement2 = (arr,k) => {
      k = k % arr.length;
    return ([...arr.slice(-k),...arr.slice(0,-k)]);
 }
-console.log(rightRotationByKElement2([1,2,3],2))
+// console.log(rightRotationByKElement2([1,2,3],2))
 
 // Third way (By Man made algorithm)
 
@@ -38,4 +38,47 @@ let rightRotationByKElement3 = (arr,k) => {
      return temp;
 }
 
-console.log(rightRotationByKElement3([1,2,3],2));
+// console.log(rightRotationByKElement3([1,2,3],2));
+
+// Right rotation by k element using two pointer approach
+
+let arr = [1,2,3,4,5];
+let k = 2;
+k = k % arr.length;
+let rightRotationByKElement4 = (i , j) => {
+  while(i<j){
+
+    // let temp = arr[i];
+    // arr[i] = arr[j];
+    // arr[j] = temp;
+
+    // Instead of above there line do this
+    [arr[i],arr[j]] = [arr[j],arr[i]];
+    i++;
+    j--;
+  }
+  return arr;
+}
+
+rightRotationByKElement4(0,arr.length-1);
+rightRotationByKElement4(0,k-1);
+rightRotationByKElement4(k,arr.length - 1);
+
+/*
+arr = [1,2,3,4,5];
+k = 2; it means have to rotate right twice 
+1st time = [5,1,2,3,4];
+2nd time = [4,5,1,2,3];
+
+****** understand here a pattern arising ********
+1st pattern => reverse whole array firstly
+[5,4,3,2,1];
+2nd pattern => reverse (0 t0 k - 1) <=> (0,1) 
+index =>  [4,5,3,2,1]
+3rd pattern => reverse (k,arr.length -1)index <=> (2,4)
+index => [4,5,1,2,3];
+ ************ Done ***********
+
+ And its' time complexity = O(n);
+ and space complexity = O(1);
+*/
--- 3_Again_Adv_Array/05_RemoveDuplicatesFromSortedArray.js
+++ 3_Again_Adv_Array/05_RemoveDuplicatesFromSortedArray.js
@@ -2,12 +2,21 @@
 
 let removeDuplicatesAndGiveUnique = (arr) => {
      let j = 1;
-     for(let i = 0; i<arr.length - 1; i++){
-          if(arr[i] != arr[i+1]){
-               arr[j] = arr[i+1];
+     for(let i = 1; i<arr.length; i++){
+          if(arr[i] != arr[i-1]){
+               arr[j] = arr[i];
                j++;
           }
      }
-     return arr;
+     return{
+          uniqueCount:j,
+          uniqueArray:arr.slice(0,j),
+     };
 }
-console.log(removeDuplicatesAndGiveUnique([0,0,1,1,2,2,3,3]));
+// console.log(removeDuplicatesAndGiveUnique([0,0,1,1,2,2,3,3]));
+
+let result = removeDuplicatesAndGiveUnique([0,0,1,1,2,2,3,3]);
+console.log("uniqueCount -: ",result.uniqueCount);
+console.log("uniqueArray -: ", result.uniqueArray);
+
+// TMC => O(n) and SPC => O(1)
